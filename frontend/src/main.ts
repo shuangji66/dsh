f71b0c143656ea04ecec1e27c24d7366733ae8b6
shuangji66 <38309970+shuangji66@users.noreply.github.com>
2026-08-23 04:24:25 +0800

    Delete frontend directory

--- frontend/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import router from './router'
-import './style.css'
-import '@xterm/xterm/css/xterm.css'
-
-createApp(App).use(createPinia()).use(router).mount('#app')
